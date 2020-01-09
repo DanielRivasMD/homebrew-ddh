d93416a885b237c116e3e1530b5acf8702d91e37
DanielRivasMD <danielrivasmd@gmail.com>
2020-01-09 00:56:04 -0600

    add formula

---
+++ ddh.rb
@@ -0,0 +1,11 @@
+class Ddh < Formula
+  desc ""
+  homepage ""
+  url "https://github.com/DanielRivasMD/ddh/files/4039270/ddh.tar.gz"
+  version "0.10.7"
+  sha256 "7d97e603168c6cec45938b9b72733709290de735e44c58bbaf57f9e7f6f6c6d8"
+
+  def install
+    bin.install "ddh"
+  end
+end
